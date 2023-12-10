639492bde0eda996d3a0fea2672d2eb59aa43468
MohammedEyadAlqwasmi <122006879+MohammedEyadAlqwasmi@users.noreply.github.com>
2023-12-10 11:13:10 +0300

    Component Autowired

--- src/main/java/com/fact/spring/ann/Master.java
+++ src/main/java/com/fact/spring/ann/Master.java
@@ -1,12 +1,31 @@
 package com.fact.spring.ann;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("master")
 public class Master implements MasterInterface{
 
+    private SportInterface sportInterface;
+
+//    @Autowired
+//    public Master(SportInterface sportInterface){
+//        this.sportInterface = sportInterface;
+//    }
+
+//    @Autowired
+//    public void setSportInterface(SportInterface sportInterface) {
+//        this.sportInterface = sportInterface;
+//    }
+
     @Override
     public void name(){
         System.out.println("I am Master");
     }
+
+    @Override
+    public void callMySport(){
+        sportInterface.mySport();
+    }
+
 }
--- src/main/java/com/fact/spring/ann/MasterInterface.java
+++ src/main/java/com/fact/spring/ann/MasterInterface.java
@@ -3,4 +3,6 @@ package com.fact.spring.ann;
 public interface MasterInterface {
 
     public void name();
+
+    public void callMySport();
 }
